eba90cfa095c84aa2d8f88a4b7af8c0e29f183aa
HossEz <41141267+HossEz@users.noreply.github.com>
2023-03-06 18:54:33 +0100

    La til header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Header from './components/Header';
 import './css/main.css';
 
 function App() {
-
+    return (
+    <Header/>
+    )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <div class="container">
+            <header>
+                <h1>RESSURSARKIV</h1>
+            </header>
+        </div>
+    )
+}
