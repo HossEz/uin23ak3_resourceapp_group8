b4e30842311051ed7328384657ba827b7806e3dc
HossEz <41141267+HossEz@users.noreply.github.com>
2023-03-06 19:12:43 +0100

    små endringer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 export default function Header(){
     return (
-        <div className="container">
+        <>
             <header>
                 <h1>RESSURSARKIV</h1>
             </header>
-        </div>
+        </>
     )
 }
