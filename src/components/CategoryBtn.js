14fb206f6fe4bb8205fd13b4177f18cee01d6de1
HossEz <41141267+HossEz@users.noreply.github.com>
2023-03-06 19:06:45 +0100

    små endring

--- src/components/CategoryBtn.js
+++ src/components/CategoryBtn.js
@@ -1,9 +1,9 @@
 export default function CategoryBtn() {
     return (
         <>
-            <button class="categorybtn">KATEGORI 1</button>
-            <button class="categorybtn">KATEGORI 2</button>
-            <button class="categorybtn">KATEGORI 3</button>
+            <button className="categorybtn">KATEGORI 1</button>
+            <button className="categorybtn">KATEGORI 2</button>
+            <button className="categorybtn">KATEGORI 3</button>
         </>
     )
 }
