288149cf6179ddcc4c0758110bf950bcae26f89d
Ephah <34236501+lilephus@users.noreply.github.com>
2023-03-09 08:52:57 +0100

    endring i app.js og index.js For router og render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
 import Header from './components/Header';
 import './css/main.css';
 import resources from './components/Resources';
-import Penger from './components/Resources';
+import Resources from './components/Resources';
 
 function App() {
     return (
     <div className="container">
         <Header/>
-        <Penger resources={resources} /> 
+        <Resources resources={resources} /> 
     </div>
     )
 }
