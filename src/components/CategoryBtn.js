3faec787e8e8ff1f7b7ce70c58db4b19e3b1dda3
Adyan <adyan850@gmail.com>
2023-03-09 10:26:46 +0100

    Rydding + merge (layout sass og components)

--- src/components/CategoryBtn.js
+++
@@ -1,9 +0,0 @@
-export default function CategoryBtn() {
-    return (
-        <>
-            <button className="categorybtn">KATEGORI 1</button>
-            <button className="categorybtn">KATEGORI 2</button>
-            <button className="categorybtn">KATEGORI 3</button>
-        </>
-    )
-}
